ca308486654c5004ad51b7c167cfcd8857fb2304
Mitch-C <mitchell.coad@hotmail.co.nz>
2016-03-15 19:48:26 +1300

    complete tutorial

--- SwenWebsite/routes/index.js
+++ SwenWebsite/routes/index.js
@@ -1,9 +1,30 @@
+//declare namespace tei='http://www.tei-c.org/ns/1.0';
+
 var express = require('express');
 var router = express.Router();
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+
+client.execute("OPEN Colenso");
+router.get("/",function(req,res){
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+" (//name[@type='place'])[1] ",
+function (error, result) {
+if(error){ console.error(error);}
+else {
+res.render('index', { title: 'ECS Video Rental', place: result.result });
+}
+}
+);
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+//});
+//try connecting to server
+console.log(1);
+
+
 
 module.exports = router;
