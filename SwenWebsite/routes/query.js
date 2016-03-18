aa4782954027a3bc290c70dbfe3958b52d912ef1
Mitch-C <mitchell.coad@hotmail.co.nz>
2016-03-18 17:38:57 +1300

    Create query view

--- SwenWebsite/routes/query.js
+++ SwenWebsite/routes/query.js
@@ -7,7 +7,7 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('query', { title: 'Colenso Project' });
 });
 
 module.exports = router;
